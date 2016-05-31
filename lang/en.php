a25be669f0ffe7885928c705f2318954de19733c
Suricat <bigben.ben@laposte.net>
2016-05-31 22:45:37 +0200

    suite

---
+++ lang/en.php
@@ -0,0 +1,12 @@
+<?php
+    $LANG = array(
+        'L_LIB_TITLE'        =>  'Title'
+       ,'L_TITLE'            =>  'RELATED POSTS'
+       ,'L_IMG_WIDTH'        =>  'Default width of the image'
+       ,'L_IMG_HEIGHT'       =>  'Default height of the image'
+       ,'L_ISCIMAGEINTHEME'  =>  'CImage tool is already in your PluXML theme (you can so delete CImage directory in this plugin)'
+       ,'L_HELP'             =>  'Add this code to the theme to display the related posts'
+       ,'L_HELP_CIMAGE'      =>  'This plugin uses CImage tool for crop images'
+       ,'L_SAVE'             =>  'Save'
+    );
+?>
